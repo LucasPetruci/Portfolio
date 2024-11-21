7f2d6696ee17bbd0966cf544b51e307f0a142a3f
LucasPetruci <lucaspetruci100@gmail.com>
2024-11-21 16:55:19 -0300

    feat: Update projects.json and enhance TechnologiesContainer with new technologies

--- src/components/TechnologiesContainer.jsx
+++ src/components/TechnologiesContainer.jsx
@@ -1,52 +1,26 @@
 import {
-  DiBootstrap,
-  DiCss3,
-  DiDocker,
-  DiGit,
-  DiHtml5,
-  DiJava,
-  DiJsBadge,
-  DiMysql,
-  DiNodejsSmall,
-  DiPostgresql,
-  DiPython,
-  DiReact,
-  DiSass,
+ DiMysql,
+ DiPython,
+
 } from "react-icons/di";
 
 import {
-  SiExpress,
   SiFlutter,
-  SiMongodb,
-  SiNestjs,
-  SiSpringboot,
-  SiSpringsecurity,
-  SiThymeleaf,
-  SiTypescript,
-  SiTailwindcss,
-  SiPrisma,
-  SiMongoose,
-  SiNextdotjs,
+  SiDart,
+  SiKotlin,
+  SiFirebase,
+ 
 } from "react-icons/si";
 
 import "../styles/components/technologiescontainer.sass";
 
 const technologies = [
-  { id: "react", name: "React", icon: <DiReact /> },
-  { id: "nextjs", name: "Next.js", icon: <SiNextdotjs /> },
-  { id: "bootstrap", name: "Bootstrap", icon: <DiBootstrap /> },
-  { id: "sass", name: "Sass", icon: <DiSass /> },
-  { id: "tailwind", name: "Tailwind CSS", icon: <SiTailwindcss /> },
-  { id: "node", name: "Node.js", icon: <DiNodejsSmall /> },
-  { id: "typescript", name: "TypeScript", icon: <SiTypescript /> },
-  { id: "nestjs", name: "NestJS", icon: <SiNestjs /> },
-  { id: "java", name: "Java", icon: <DiJava /> },
-  { id: "springboot", name: "Spring Boot", icon: <SiSpringboot /> },
   { id: "flutter", name: "Flutter", icon: <SiFlutter /> },
-  { id: "docker", name: "Docker", icon: <DiDocker /> },
-  { id: "mysql", name: "MySQL", icon: <DiMysql /> },
-  { id: "mongodb", name: "MongoDB", icon: <SiMongodb /> },
-  { id: "postgresql", name: "PostgreSQL", icon: <DiPostgresql /> },
+  { id: "dart", name: "Dart", icon: <SiDart /> },
+  {id: "kotlin", name: "Kotlin", icon: <SiKotlin/>},
+  {id: "firebase", name: "Firebase", icon: <SiFirebase/>},
+  {id: "mysql", name: "MySQL", icon: <DiMysql/>},
+  {id: "python", name: "Python", icon: <DiPython/>}
 ];
 
 const TechnologiesContainer = () => {
