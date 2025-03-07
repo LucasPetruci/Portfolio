92e366b3cf6c561bbb7d21000a0f0fc232898709
LucasPetruci <lucaspetruci100@gmail.com>
2025-03-07 18:58:47 -0300

    feat: update and add new expirience

--- src/components/ExperienceContainer.jsx
+++ src/components/ExperienceContainer.jsx
@@ -9,43 +9,26 @@ const ExperienceContainer = () => {
       company:
         "Polo de Inovação Campos dos Goytacazes (PICG) - SoftSeg e Embrapii",
       date: "Abril/2024 - Atualmente",
-      tasks: `
-      • Responsável pelo desenvolvimento de interfaces em Flutter para aplicativos móveis com base layout no Figma.
-      • Colaboração com a equipe de design e back-end para criar uma experiência de usuário intuitiva.
-      • Responsável por corrigir bugs e otimizar o desempenho das aplicações.
-      • Integração com API do Google.
-      • Criação de código em linguagem nativa para comunicação com Flutter.
-      • Utilização de Design Pattern - MVC.
-      • Utilização dos princípios de SOLID.
-      • Consumo de APIs em RESTful com package HTTP.
-      • Gerenciamento de estado e injeção de dependência com Provider.
-      • Versionamento de código com Git.
-      • Utilização de deep linking.
-      • Experiência em Kanban como modelo ágil.
-      `,
+      tasks: `Atuo no desenvolvimento da aplicação mobile utilizando Flutter, criando soluções responsivas e bem estruturadas para proporcionar uma experiência de usuário intuitiva e eficiente. Em colaboração com a equipe de design e back-end, transformo protótipos em interfaces funcionais, garantindo que os aplicativos atendam aos requisitos técnicos e visuais. Sou responsável pela implementação de funcionalidades de geolocalização, integrando Google Maps e Geocoding para oferecer serviços baseados em localização de forma precisa e eficiente. Além disso, desenvolvo módulos nativos em Kotlin para aprimorar o suporte a GPS e funcionalidades específicas do Android, garantindo maior compatibilidade e desempenho. Para assegurar a estabilidade e qualidade do software, realizo consumo de APIs RESTful, gerenciamento de estado da aplicação com Provider e utilizo Firebase Crashlytics para monitoramento e resolução de erros, contribuindo para a otimização contínua da aplicação.`,
+    },
+     {
+      title: "Desenvolvedor Full Stack",
+      company:
+        "JáCredenciei e FsTech",
+      date: "Janeiro/2025 - Atualmente",
+       tasks: `Desenvolvo e mantenho a aplicação mobile utilizando React Native, garantindo alto desempenho, fluidez e uma experiência de usuário otimizada. Além disso, atuo na manutenção e evolução de sistemas web em PHP, focando na otimização do código e na implementação de novas funcionalidades para melhorar a eficiência e a segurança das aplicações. Sou responsável pela integração de APIs RESTful, assegurando a comunicação eficaz entre os sistemas e o gerenciamento de bancos de dados. Para garantir escalabilidade e alta disponibilidade, utilizo AWS para hospedagem e otimização de infraestrutura, proporcionando soluções mais estáveis e eficientes.`,
     },
     {
       title: "Analista de Dados",
       company: "Polo de Inovação Campos dos Goytacazes (PICG) - T&D Suntentável",
       date: "Setembro/2023 a Abril/2024",
-      tasks:
-      `
-      • Responsável por extração e análise de dados sobre consumo de água.
-      • Extração de dados de planilha em python, utilizando a biblioteca pandas.
-      • Persistência de dados em MySql.
-      • Utilização de regressão linear para predição de novos dados.
-      `,
+      tasks: `Realizei a extração e análise de grandes volumes de dados utilizando Python, aplicando bibliotecas como Pandas e NumPy para processamento, tratamento e visualização de informações de forma eficiente. Desenvolvi modelos preditivos baseados em Regressão Linear, permitindo a identificação de padrões e a antecipação de tendências para otimização de processos. Além disso, otimizei consultas SQL em MySQL, melhorando a performance no acesso e manipulação dos dados, o que facilitou a geração de relatórios interativos e a extração de insights estratégicos para embasar a tomada de decisões dos stakeholders.`,
     },
     {
       title: "Q&A",
       company: "Polo de Inovação Campos dos Goytacazes (PICG)",
       date: "Agosto/2022 a Setembro/2023",
-      tasks:
-      `
-      • Responsável pela atualização da Biblioteca ShouldDSL para Python 3.
-      • Teste de software utilizando TDD.
-      • Integração com Pytest para gerar relatório em html
-      `,
+      tasks: `Contribuí para a modernização da Biblioteca ShouldDSL, migrando seu código para compatibilidade com Python 3 e garantindo sua funcionalidade em versões mais recentes da linguagem. Além disso, implementei testes automatizados seguindo a metodologia TDD, aumentando a confiabilidade e robustez do desenvolvimento. Trabalhei na integração de testes utilizando Pytest, permitindo a geração de relatórios detalhados em HTML para análise da qualidade do software e identificação de possíveis melhorias, contribuindo para um processo de desenvolvimento mais eficiente e estruturado.`,
     },
   ];
 
