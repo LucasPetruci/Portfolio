dc8f9427a343269517686a8fa0fcb273854e52cb
LucasPetruci <lucaspetruci100@gmail.com>
2025-12-31 18:48:43 -0300

    feat: update TechnologiesContainer with new technologies and their icons, enhance styles for better visual representation

--- src/components/TechnologiesContainer.jsx
+++ src/components/TechnologiesContainer.jsx
@@ -7,6 +7,15 @@ import {
 import {
   SiPhp,
   SiReact,
+  SiNodedotjs,
+  SiJavascript,
+  SiHtml5,
+  SiCss3,
+  SiAntdesign,
+  SiAwslambda,
+  SiLaravel,
+  SiAmazonsqs,
+  SiCakephp
 } from "react-icons/si";
 
 import { FaAws } from "react-icons/fa";
@@ -16,29 +25,25 @@ import { FaGithub, FaDocker, FaGitAlt} from "react-icons/fa";
 import "../styles/components/technologiescontainer.sass";
 
 const technologies = [
+  { id: "antd", name: "Ant Design (AntD)", icon: <SiAntdesign /> },
   { id: "aws", name: "AWS", icon: <FaAws /> },
+  { id: "aws_lambda", name: "AWS Lambda", icon: <SiAwslambda /> },
+  { id: "aws_sqs", name: "AWS SQS", icon: <SiAmazonsqs /> },
+  { id: "cakephp", name: "CakePHP", icon: <SiCakephp /> },
+  { id: "css", name: "CSS", icon: <SiCss3 /> },
   { id: "docker", name: "Docker", icon: <FaDocker /> },
   { id: "git", name: "Git", icon: <FaGitAlt /> },
   { id: "github", name: "GitHub", icon: <FaGithub /> },
+  { id: "html", name: "HTML", icon: <SiHtml5 /> },
+  { id: "javascript", name: "JavaScript", icon: <SiJavascript /> },
+  { id: "laravel", name: "Laravel", icon: <SiLaravel /> },
   { id: "mysql", name: "MySQL", icon: <DiMysql /> },
+  { id: "node", name: "Node.js", icon: <SiNodedotjs /> },
   { id: "php", name: "PHP", icon: <SiPhp /> },
   { id: "python", name: "Python", icon: <DiPython /> },
   { id: "react", name: "React", icon: <SiReact /> },
-  { id: "react_native", name: "React Native", icon: <SiReact /> },
-
-    // 🔽 Tecnologias usadas no trabalho (ainda sem ícone/import)
-  // { id: "node", name: "Node.js" },
-  // { id: "javascript", name: "JavaScript" },
-  // { id: "html", name: "HTML" },
-  // { id: "css", name: "CSS" },
-  // { id: "antd", name: "Ant Design (AntD)" },
-  // { id: "cakephp", name: "CakePHP" },
-  // { id: "eloquent", name: "Eloquent ORM" },
-  // { id: "ci_cd", name: "CI/CD" },
-  // { id: "aws_lambda", name: "AWS Lambda" },
-  // { id: "expo", name: "Expo" },
-  // { id: "pagbank", name: "Checkout (PagBank)" },
 ];
+
 
 
 const TechnologiesContainer = () => {
